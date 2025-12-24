f722ad58db05eac6c3373898131969a6b7fca23d
vaijaaaaa <vaijuwalker111@gmail.com>
2025-12-24 18:31:25 +0530

    feat: implement submission handling and retrieval endpoints with error handling

---
+++ app/api/submissions/[id]/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
+import {prisma} from '@/lib/prisma';
+import { submissionQueue } from "@/lib/queue/submission-queue";
+import { error } from "console";
+
+export async function POST (request : Request){
+    try {
+        const session = await getServerSession(authOptions);
+
+        if(!session || !session.user){
+            return NextResponse.json(
+                {error:'Unauthorized'},
+                {status : 401}
+            );
+        }
+
+        const body = await request.json();
+        const {problemId,query} = body;
+
+        if(!problemId || !query){
+            return NextResponse.json(
+                {error:'Problem ID and query are required'},
+                {status:400}
+            )
+        }
+
+        const problem = await prisma.problem.findUnique({
+            where : {id : problemId}
+        })
+
+        if(!problem){
+            return NextResponse.json(
+                {error : 'Problem not found'},
+                {status : 404}
+            );
+        }
+
+        const submission = await prisma.submission.create({
+            data : {
+                userId : session.user.id,
+                problemId,
+                query,
+                isCorrect:false,
+            }
+        })
+
+        await submissionQueue.add({
+            submissionId : submission.id,
+            userId : session.user.id,
+            problemId,
+            query,
+        })
+
+        return NextResponse.json({
+            message : 'submission received',
+            submissionId : submission.id,
+        },{status : 202})
+
+    } catch (error) {
+        console.error('Submission error :',error);
+        return NextResponse.json(
+            {error : 'Internal server error'},
+            {status : 500}
+        )
+    }
+}
